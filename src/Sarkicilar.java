91c59527458746a7e99377fc4757fdd48d70bd94
berkbahadir34 <berkbahadir34@gmail.com>
2020-10-10 19:43:10 +0300

    ArrayListler Kullanılarak Şarkıcılar Programı

---
+++ src/Sarkicilar.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Sarkicilar {
+    private ArrayList<String> sarkici_listesi = new ArrayList<String>();
+
+    public void sarkicilari_bastir(){
+        System.out.println("Şarkıcı Listesinde "+ sarkici_listesi.size()+" Kadar Şarkıcı Var..");
+
+        for(int i=0; i<sarkici_listesi.size();i++){
+            System.out.println((i+1)+" . Şarkıcı "+sarkici_listesi.get(i));
+        }
+    }
+    public void sarkici_ekle(String isim){
+        sarkici_listesi.add(isim);
+        System.out.println("Şarkıcı Listesi Güncellendi....");
+    }
+    public void sarkici_guncelle(String yeni_isim, int pozisyon){
+        sarkici_listesi.set(pozisyon,yeni_isim);
+        System.out.println("Şarkıcı Listesi Güncellendi...");
+
+    }
+    public void sarkici_sil(int pozisyon){
+        String isim =sarkici_listesi.get(pozisyon);
+        sarkici_listesi.remove(pozisyon);
+        System.out.println(isim+" isimli şarkıcı listeden silindi....");
+
+    }
+    public void sarkici_ara(String sarkici){
+        int pozisyon =sarkici_listesi.indexOf(sarkici);
+        if(pozisyon>=0){
+            System.out.println("Şarkıcı Bulundu");
+            System.out.println(sarkici+ " isimli şarkıcı"+(pozisyon+1)+". pozisyonda..");
+        }
+        else{
+            System.out.println("Şarkıcı bulunamadı...");
+        }
+    }
+}
